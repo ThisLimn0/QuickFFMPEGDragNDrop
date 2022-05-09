0856837a2c8bd802cc97781b5ec5b3aae9722ae8
Limn0 <67125885+ThisLimn0@users.noreply.github.com>
2022-05-09 16:20:42 +0200

    Add files via upload

---
+++ QuickFFMPEGDnD.bat
@@ -0,0 +1,43 @@
+@ECHO OFF & SETLOCAL EnableDelayedExpansion
+TITLE QuickFFMPEG to MP4
+SET /A LastArg=0
+FOR %%A in (%*) DO (
+    IF NOT [%%A]==[] (
+        SET /A LastArg+=1
+        SET "INPUT!LastArg!=%%A"
+    )
+)
+
+REM Check if ffmpeg.exe is available
+IF NOT EXIST "%~dp0ffmpeg.exe" (
+    ECHO.ffmpeg.exe is not available at the script location.
+    TIMEOUT /T 5 /NOBREAK >NUL
+    EXIT /B 1
+)
+
+REM Check if progrem was not ran with drag and drop
+IF !LastArg! EQU 0 (
+    ECHO.This program cannot be called directly.
+    TIMEOUT /T 5 /NOBREAK >NUL
+    EXIT /B 42
+)
+
+ECHO.Provided !LastArg! files to process.
+
+FOR /l %%A IN (1,1,!LastArg!) DO (
+    TITLE Processing file %%A of !LastArg!
+    ECHO.Input file: !INPUT%%A!
+    SET "OUTPUT=!INPUT%%A:~0,-4!.mp4"
+    %~dp0ffmpeg.exe -i "!INPUT%%A!" "!OUTPUT!"
+    IF NOT %ERRORLEVEL% EQU 0 (
+       ECHO. ---
+       ECHO.There was an error in the process of the conversion.
+       TIMEOUT /T 5 /NOBREAK >NUL
+    )
+)
+TITLE Processed !LastArg! files.
+ECHO. ---
+ECHO.Conversion has finished.
+TIMEOUT /T 5 /NOBREAK >NUL
+
+

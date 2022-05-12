bb93368f9071cb6a94d6e42525b2de400f6fc462
Limn0 <67125885+ThisLimn0@users.noreply.github.com>
2022-05-12 10:57:38 +0200

    Update QuickFFMPEGDnD.bat
    fixing weird ffmpeg whitespace conventions and better error handling for multiple files.

--- QuickFFMPEGDnD.bat
+++ QuickFFMPEGDnD.bat
@@ -4,7 +4,7 @@ SET /A LastArg=0
 FOR %%A in (%*) DO (
     IF NOT [%%A]==[] (
         SET /A LastArg+=1
-        SET "INPUT!LastArg!=%%A"
+        SET "INPUT!LastArg!="%%~A""
     )
 )
 
@@ -15,7 +15,7 @@ IF NOT EXIST "%~dp0ffmpeg.exe" (
     EXIT /B 1
 )
 
-REM Check if progrem was not ran with drag and drop
+REM Check if program was not ran with drag and drop
 IF !LastArg! EQU 0 (
     ECHO.This program cannot be called directly.
     TIMEOUT /T 5 /NOBREAK >NUL
@@ -27,17 +27,29 @@ ECHO.Provided !LastArg! files to process.
 FOR /l %%A IN (1,1,!LastArg!) DO (
     TITLE Processing file %%A of !LastArg!
     ECHO.Input file: !INPUT%%A!
-    SET "OUTPUT=!INPUT%%A:~0,-4!.mp4"
-    %~dp0ffmpeg.exe -i "!INPUT%%A!" "!OUTPUT!"
-    IF NOT %ERRORLEVEL% EQU 0 (
-       ECHO. ---
-       ECHO.There was an error in the process of the conversion.
-       TIMEOUT /T 5 /NOBREAK >NUL
+    SET "OUTPUT=!INPUT%%A:~0,-5!.mp4""
+    ECHO.Output file: !OUTPUT!
+    %~dp0ffmpeg.exe -i !INPUT%%A! -map 0 !OUTPUT!
+    IF NOT !ERRORLEVEL! EQU 0 (
+        SET "ERRMSG=!ERRMSG!     -!INPUT%%A!"
+        ECHO. ---
+        ECHO.ERROR: !ERRORLEVEL!
+        ECHO.There was an error in the process of the conversion.
+        TIMEOUT /T 2 /NOBREAK >NUL
     )
 )
 TITLE Processed !LastArg! files.
 ECHO. ---
-ECHO.Conversion has finished.
-TIMEOUT /T 5 /NOBREAK >NUL
-
+IF NOT DEFINED ERRMSG (
+    ECHO.Conversion has finished.
+    TIMEOUT /T 5 /NOBREAK >NUL
+    PAUSE >NUL
+    EXIT /B 0
+) ELSE (
+    ECHO.Conversion has finished, however there was an error with the following files:
+    ECHO.!ERRMSG!
+    TIMEOUT /T 5 /NOBREAK >NUL
+    PAUSE >NUL
+    EXIT /B 0
+)
 
